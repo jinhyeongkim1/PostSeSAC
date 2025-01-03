a811a0369010fa8c3cf202e4dd144d219c58143a
jinhyeongkim <jinhyeong_kim@naver.com>
2025-01-03 18:14:34 +0900

    vue counter구현

--- 241219_work/Vue/src/App.vue
+++ 241219_work/Vue/src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="app">
     <h1>Hello SeSAC~~</h1>
+    <h1>Counter</h1>
+    <p>{{ count }}</p>
+    <button class="increment-btn" @click="increment">더하기</button>
+    <button class="decrement-btn" @click="decrement">빼기</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    }
+  }
 }
 </script>
 
@@ -18,5 +35,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  margin: 5px;
+  padding: 10px;
+}
+.increment-btn {
+  background-color: blue;  
+  color: white;
+}
+.decrement-btn {
+  background-color: red;   
+  color: white;
 }
 </style>
